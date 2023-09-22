a182f5c184d8d61ee1d03773781dbbcd1602f172
dolphin <dolphin@163.com>
2023-09-22 18:25:18 +0800

    add:Component声明

---
+++ 04-spring-boot-bean/src/main/java/io/dolphin/bean/annotate/StudentAnnotateService.java
@@ -0,0 +1,11 @@
+package io.dolphin.bean.annotate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentAnnotateService {
+
+    public String hello() {
+        return "hello";
+    }
+}
